951c96d3f7b76af7500ef867331e2805641ac1dd
daborgo <diegoborgo14@gmail.com>
2025-10-30 17:25:42 -0700

    Initial Commit

---
+++ GamePlan/GamePlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamePlanApp.swift
+//  GamePlan
+//
+//  Created by Diego Borgo on 10/30/25.
+//
+
+import SwiftUI
+
+@main
+struct GamePlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
