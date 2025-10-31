7a3da58b2e43237e295ed4f081215e9d7d32f9a1
daborgo <diegoborgo14@gmail.com>
2025-10-30 22:51:38 -0700

    add and delete event implementation

--- GamePlan/EventView.swift
+++ GamePlan/EventView.swift
@@ -11,6 +11,20 @@ struct EventView: View {
 //    @StateObject var data = FinanceData()
     
     @State var searchText: String = ""
+    
+    @State var addEventName: String = ""
+    @State var addEventDesc: String = ""
+    @State var addEventDate: String = ""
+    @State var addEventTime: String = ""
+    @State var addEventLoc: String = ""
+    
+    @State var deleteEventName: String = ""
+    
+    @State var showingAddEvent: Bool = false
+    @State var showingDeleteEvent: Bool = false
+    
+    @State var err: String = ""
+    @State var deleteErr: String = ""
 
     var body: some View {
         NavigationStack {
@@ -25,10 +39,10 @@ struct EventView: View {
                 }.background(Color.gray.opacity(0.5)).cornerRadius(8)
                 HStack {
                     Button("Add Event", action: {
-                        
+                        showingAddEvent = true
                     }).buttonStyle(.borderedProminent)
                     Button("Delete Event", action: {
-                        
+                        showingDeleteEvent = true
                     }).buttonStyle(.borderedProminent)
                 }
                 searchBar.padding(.top, 12)
@@ -38,6 +52,95 @@ struct EventView: View {
             .navigationTitle("Event View")
             .containerRelativeFrame([.horizontal, .vertical])
             .background(Gradient(colors: [.cyan, .purple, .purple, .purple]).opacity(0.5))
+            .sheet(isPresented: $showingAddEvent) {
+                NavigationStack {
+                    VStack {
+                        HStack {
+                            Text("Name: ")
+                            TextField("Name", text: $addEventName)
+                        }
+                        HStack {
+                            Text("Description: ")
+                            TextField("Description", text: $addEventDesc)
+                        }
+                        HStack {
+                            Text("Date: ")
+                            TextField("Date", text: $addEventDate)
+                        }
+                        HStack {
+                            Text("Time: ")
+                            TextField("Time", text: $addEventTime)
+                        }
+                        HStack {
+                            Text("Location: ")
+                            TextField("Location", text: $addEventLoc)
+                        }
+                        Button("Add", action: {
+                            addEventName = ""
+                            addEventDesc = ""
+                            addEventDate = ""
+                            addEventTime = ""
+                            addEventLoc = ""
+                            showingAddEvent = false
+                        }).buttonStyle(.borderedProminent)
+                        Text(err).padding().foregroundColor(.red)
+                    }
+                    .padding()
+                    .navigationBarTitleDisplayMode(.inline)
+                    .navigationTitle("Add Event")
+                    .toolbar{
+                        ToolbarItem(placement: .navigationBarLeading) {
+                            Button(action: {
+                                addEventName = ""
+                                addEventDesc = ""
+                                addEventDate = ""
+                                addEventTime = ""
+                                addEventLoc = ""
+                                err = ""
+                                showingAddEvent = false
+                            }, label: {
+                                Text("Cancel")
+                            })
+                        }
+                    }
+                }
+            }
+            .sheet(isPresented: $showingDeleteEvent) {
+                NavigationStack {
+                    VStack {
+                        HStack {
+                            Text("Name: ")
+                            TextField("Name", text: $deleteEventName)
+                        }
+                        Button("Search", action: {
+                            deleteErr = "Found event!"
+                        }).buttonStyle(.borderedProminent)
+                        Text(deleteErr).padding().foregroundColor(.green)
+                        Button("Delete", action: {
+                            deleteEventName = ""
+                            deleteErr = ""
+                            err = ""
+                            showingDeleteEvent = false
+                        }).buttonStyle(.borderedProminent).padding(.top, 12)
+                        Text(err).padding().foregroundColor(.red)
+                    }
+                    .padding()
+                    .navigationBarTitleDisplayMode(.inline)
+                    .navigationTitle("Delete Event")
+                    .toolbar{
+                        ToolbarItem(placement: .navigationBarLeading) {
+                            Button(action: {
+                                deleteEventName = ""
+                                deleteErr = ""
+                                err = ""
+                                showingDeleteEvent = false
+                            }, label: {
+                                Text("Cancel")
+                            })
+                        }
+                    }
+                }
+            }
         }.tint(.purple)
     }
     
